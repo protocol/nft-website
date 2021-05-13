e826b88f4bf9d268c8105e08bc967ff7bfefcd2d
John Mulhausen <jmulhausen@digitalocean.com>
2021-05-13 18:25:34 -0400

    CI check improvements (#49)
    * CI check improvements
    
    * Update cicheck.sh
    
    * Test SVG
    
    * git logic
    
    * Automatically optimized images.
    
    * Reinstate CI skip for image optimization, remove test SVG
    
    Co-authored-by: filecorgi <john.mulhausen@protocol.ai>

--- scripts/cicheck.sh
+++ scripts/cicheck.sh
@@ -2,6 +2,10 @@
 
 git config --global user.name "$GH_USERNAME"
 git config --global user.email "$GH_USER_EMAIL"
+git remote add fdocs https://$${GH_TOKEN}@github.com/protocol/nft-website.git > /dev/null 2>&1
+git fetch fdocs
+git checkout --track origin/$PR_HEAD_REF
+git pull
 
 BRANCHNAME='$PR_HEAD_REF'
 echo "Testing on commit range: $FIRST_COMMIT..$LAST_COMMIT"
@@ -63,15 +67,11 @@ if [ -z "$(git status --porcelain)" ]; then
 - Image optimization came back clean!"
 else
   # Uncommitted changes
-  git remote add fdocs https://$${GH_TOKEN}@github.com/protocol/nft-website.git > /dev/null 2>&1
-  git fetch fdocs
-  git checkout --track origin/$PR_HEAD_REF
-  git pull
   git add .
-  git commit -m "Automatically optimized images. [ciskip]"
+  git commit -m "Automatically optimized images [ci skip]."
   git push
   COMMENT="$COMMENT
-- I optimized some images for you! See the commit with the comment \`Automatically optimized images [ciskip]\` in this PR for details."
+- I optimized some images for you! See the commit with the comment \`Automatically optimized images [ci skip].\` in this PR for details."
 fi
 
 echo "Run npm install"
