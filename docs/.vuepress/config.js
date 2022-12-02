5ab5b3a960ad13e628a2fe3e06f19286f89893ab
Adam Munns <hello@munnsmedia.com>
2022-12-02 07:47:47 -0800

    fix: formatting diff

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -24,9 +24,9 @@ module.exports = {
       md.use(require('markdown-it-video'))
       md.use(require('markdown-it-footnote'))
       md.use(require('markdown-it-task-lists'))
-      md.use(require('markdown-it-deflist')),
-        md.use(require('markdown-it-imsize')),
-        md.use(require('markdown-it-image-lazy-loading'))
+      md.use(require('markdown-it-deflist'))
+      md.use(require('markdown-it-imsize'))
+      md.use(require('markdown-it-image-lazy-loading'))
     }
   },
   themeConfig: {
